7ebdc897b261d519b87e94b1ce2354672bf1cf1a
tonyliuzj <email@tonyliu.uk>
2026-06-09 15:49:57 +0100

    fix: update toaster position to top-center for improved visibility

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -6,7 +6,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       className="toaster group"
-      position="top-right"
+      position="top-center"
       richColors
       toastOptions={{
         classNames: {
